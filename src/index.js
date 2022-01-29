86f836e4f02629cd9bd003b2a4bb476132bc4cd3
152334H <54623771+152334H@users.noreply.github.com>
2022-01-29 03:31:13 +0000

    Deploying to gh-pages from @ 152334H/react-viewer-viewer@98bd1087dbe08036e00b36f07124cd46d25a14cd 🚀

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// placeholder
-import * as React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-setTimeout (() => {
-  ReactDOM.render(<App />, document.getElementById('root'));
-}, 3000);
