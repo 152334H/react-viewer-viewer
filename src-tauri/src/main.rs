8d70feea293a8107ad3825ccd1ec4c98d39c9ee5
152334H <54623771+152334H@users.noreply.github.com>
2022-01-29 16:09:32 +0800

    add a test command to tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,14 @@
   windows_subsystem = "windows"
 )]
 
+mod lib;
+use lib::{JSON_viewerstate, test_thing};
+
+#[tauri::command]
+fn compile_images(json: JSON_viewerstate) -> i32 {
+    test_thing(json)
+}
+
 fn main() {
   tauri::Builder::default()
     .run(tauri::generate_context!())
