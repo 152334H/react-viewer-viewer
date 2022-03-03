abf5d79fcbbbca5d476f23b68072d4724bd16739
152334H <54623771+152334H@users.noreply.github.com>
2022-03-03 13:53:46 +0800

    remove todos

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,7 +9,6 @@
  * - make a git push hook so I don't forget to bump the version number every time?
  * - increase speed more somehow
  * - also, compilation speed
- * - separate function to return uncompressed images instead of zip
  */
 
 mod lib;
@@ -20,7 +19,6 @@ async fn compile_compressed_images(json_images: JSONImages, zoom: f64) -> Result
     compressed_viewer_to_zip(json_images, zoom).map_err(|e| e.to_string())
 }
 
-// TODO: test this command so that we can move on with the "send a compressed state" plan
 #[tauri::command]
 async fn flatten_images(comp_imgs: Vec<String>, deref_img_states: Vec<JSONCompressedImgState>) -> Result<Vec<Vec<u8>>, String> {
     compressed_viewer_to_flat(JSONImages { dataURLs: comp_imgs, imgStates: deref_img_states }).map_err(|e| e.to_string())
